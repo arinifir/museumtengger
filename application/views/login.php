6220b439fd9107d9e42686e19088631bc1719c10
arinifir <55473280+arinifir@users.noreply.github.com>
2021-04-21 15:34:27 +0700

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -6,7 +6,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<!-- <link rel="stylesheet" href="<?= base_url('assets/admin/') ?>assets/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="<?= base_url('assets/') ?>mycss/my.css">
-    <title>Login Form Using HTML And CSS Only</title>
 </head>
 
 <body>
